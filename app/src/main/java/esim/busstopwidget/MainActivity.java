9362814634bad57c82e7fa522628dba7da9b3106
hvilmi <hannes.vilmi@gmail.com>
2017-03-01 17:35:04 +0200

    url parsing base

--- app/src/main/java/esim/busstopwidget/MainActivity.java
+++ app/src/main/java/esim/busstopwidget/MainActivity.java
@@ -4,21 +4,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
 
+//API KEY: AIzaSyBN2rnahDKnRnQpFQOGUC_S123FeRy6shE
 
 public class MainActivity extends AppCompatActivity {
     GoogleApiClient mGoogleApiClient;
+    String base="https://maps.googleapis.com/maps/api/directions/json?";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String place = "disneyland";
+        base=base+"origin="+place;
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /*FragmentActivity*/,
-                                  this /*OnConnectionFailedListener*/)
-                .addApi(DirectionsService)
-                .build();
+
     }
 }
